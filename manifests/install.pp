fae390582480e54703fd0df8bf131f8c360b2471
P. Gomersbach <p.gomersbach@rely.nl>
2017-04-20 11:56:04 +0000

    set public hostname

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,12 +8,25 @@ class profile_rundeck::install {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
+  if $::ec2_public_ipv4 != undef {
+    $public_hostname = $::ec2_public_ipv4
+  }
+  else
+  {
+    if $::ec2_metadata != undef {
+      $public_hostname = $::facts['ec2_metadata']['public-ipv4']
+    }
+    else {
+      $public_hostname = $::fqdn
+    }
+  }
   class { 'java':
     distribution => 'jdk',
   }
   class {'rundeck':
-    package_ensure => '2.6.11',
-    require        => Class['java'],
+    package_ensure     => '2.6.11',
+    server_web_context => "http://${public_hostname}:4440",
+    require            => Class['java'],
   }
 
 }
