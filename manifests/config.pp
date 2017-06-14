d70fd693026e7777d4a7581a38887c48e03affc7
P. Gomersbach <p.gomersbach@rely.nl>
2017-06-14 13:04:00 +0000

    add job

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,21 +8,35 @@ class profile_rundeck::config {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
-  file { '/var/lib/rundeck/rundeckmcoinv.mco':
+  file {'/var/lib/rundeck/rundeckmcoinv.mco':
     source  => 'puppet:///modules/profile_rundeck/rundeckmcoinv.mco',
     require => Class['rundeck'],
   }
 
-  file { '/tmp/projects/':
+  file {'/tmp/projects/':
     ensure  => directory,
     recurse => true,
     source  => 'puppet:///modules/profile_rundeck/projects',
     require => Class['rundeck'],
   }
 
-  exec { 'move projectsdir':
+  exec {'move projectsdir':
     command     => '/bin/rm -rf /var/lib/rundeck/projects;/bin/mv /tmp/projects /var/lib/rundeck/',
     subscribe   => File['/tmp/projects/'],
+    refreshonly => true,
+  }
+
+  file {'/tmp/jobs/':
+    ensure  => directory,
+    recurse => true,
+    source  => 'puppet:///modules/profile_rundeck/jobs',
+    require => Class['rundeck'],
+  }
+
+  exec { 'inport check_puppet_resources job':
+    command     => 'RD_USER=admin RD_PASSWORD=admin RD_URL=http://localhost:4440 /usr/bin/rd jobs load --duplicate update --format yaml --project Management --file /tmp/jobs/check_puppet_resources',
+    path        => '/usr/bin',
+    subscribe   => File['/tmp/jobs/'],
     refreshonly => true,
   }
 
