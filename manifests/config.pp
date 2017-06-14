dbf278edf640b061d143151edbd94c80a83abf7e
P. Gomersbach <p.gomersbach@rely.nl>
2017-06-14 12:29:44 +0000

    delete target dir before moving

--- manifests/config.pp
+++ manifests/config.pp
@@ -21,7 +21,7 @@ class profile_rundeck::config {
   }
 
   exec { 'move projectsdir':
-    command     => '/bin/mv -u /tmp/projects /var/lib/rundeck/',
+    command     => '/bin/rm -rf /var/lib/rundeck/projects;/bin/mv /tmp/projects /var/lib/rundeck/',
     subscribe   => File['/tmp/projects/'],
     refreshonly => true,
   }
